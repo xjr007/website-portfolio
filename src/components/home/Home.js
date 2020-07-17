5b3497434bdbbb602d5934e1e42f4adfbfe55cd5
Aaqiel <abaaqielbehardien@gmail.com>
2020-07-17 10:05:05 +0200

    about home component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,7 +6,7 @@ const Home = props => {
 		<div>
 			<header>
 				<div className='heading'>
-					<h2>Aaqiel Behardien</h2>
+					<h1>Aaqiel Behardien</h1>
 					<p>Junior Web Developer</p>
 					<button className='button' type='button'>
 						<span>Explore</span>
@@ -14,7 +14,46 @@ const Home = props => {
 				</div>
 			</header>
 			<main>
-				<h1>Main</h1>
+				<div className='about'>
+					<h2>About</h2>{' '}
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi libero ab, commodi veritatis
+						perferendis excepturi eius temporibus voluptatum. Iusto, quaerat?
+					</p>
+					<div className='navigation'>
+						<ul className='cards'>
+							<li>
+								<div className='card'>
+									<a href='/' type='button' className='button'>
+										Resume
+									</a>
+									<p>
+										Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quos ducimus, facilis alias
+										quisquam voluptatibus?
+									</p>
+								</div>
+								<div className='card'>
+									<a href='/' type='button' className='button'>
+										Testimonials
+									</a>
+									<p>
+										Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quos ducimus, facilis alias
+										quisquam voluptatibus?
+									</p>
+								</div>
+								<div className='card'>
+									<a href='/' type='button' className='button'>
+										Projects
+									</a>
+									<p>
+										Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quos ducimus, facilis alias
+										quisquam voluptatibus?
+									</p>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
 			</main>
 			<footer>
 				<h1>Footer</h1>
