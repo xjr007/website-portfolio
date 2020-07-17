75997f3fb66c8d2d4ceb13a57a8095a397970033
Aaqiel <abaaqielbehardien@gmail.com>
2020-07-17 09:21:18 +0200

    home component header

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,26 @@
-$('.box').on('mouseenter', function () {
-	var duration = 1;
-	TweenMax.to(this, duration / 4, { y: -50, ease: Power2.easeOut });
-	TweenMax.to(this, duration / 2, { y: 0, ease: Bounce.easeOut, delay: duration / 4 });
-});
+import React from 'react';
+import './Home.css';
+
+const Home = props => {
+	return (
+		<div>
+			<header>
+				<div className='heading'>
+					<h2>Aaqiel Behardien</h2>
+					<p>Junior Web Developer</p>
+					<button className='button' type='button'>
+						<span>Explore</span>
+					</button>
+				</div>
+			</header>
+			<main>
+				<h1>Main</h1>
+			</main>
+			<footer>
+				<h1>Footer</h1>
+			</footer>
+		</div>
+	);
+};
+
+export default Home;
