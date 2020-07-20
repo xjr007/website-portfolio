fe2695eb541ca266dc5ee30352965254f9d682b8
Aaqiel <abaaqielbehardien@gmail.com>
2020-07-20 12:14:15 +0200

    Updates

---
+++ precache-manifest.f9a33ab291f0adf66d6318a09fac4e64.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96524aebc51156e638ea57377a1b4b60",
+    "url": "/website-portfolio/index.html"
+  },
+  {
+    "revision": "c2b3a9f70ea0dba8ff5b",
+    "url": "/website-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "38a94a80586b6ee79dba",
+    "url": "/website-portfolio/static/css/main.099ab5ea.chunk.css"
+  },
+  {
+    "revision": "c2b3a9f70ea0dba8ff5b",
+    "url": "/website-portfolio/static/js/2.ffa347d2.chunk.js"
+  },
+  {
+    "revision": "84925f79d5e4433ea87e40b57227d707",
+    "url": "/website-portfolio/static/js/2.ffa347d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38a94a80586b6ee79dba",
+    "url": "/website-portfolio/static/js/main.5ec87e46.chunk.js"
+  },
+  {
+    "revision": "7765f274dd306fdb1d92",
+    "url": "/website-portfolio/static/js/runtime-main.16174c02.js"
+  },
+  {
+    "revision": "47e62727806289606bcbc8e0239670fa",
+    "url": "/website-portfolio/static/media/djangoIcon.47e62727.svg"
+  },
+  {
+    "revision": "0c0d0a7bc23460d052f6b87639c86d46",
+    "url": "/website-portfolio/static/media/githubIcon.0c0d0a7b.svg"
+  },
+  {
+    "revision": "62593ffa4c7bb7261c7f40a15fb164c3",
+    "url": "/website-portfolio/static/media/instagramIcon.62593ffa.svg"
+  },
+  {
+    "revision": "5b54675950638dd95de282256b5cf7c4",
+    "url": "/website-portfolio/static/media/javascriptIcon.5b546759.svg"
+  },
+  {
+    "revision": "56b93e754e74fe9b03b4f3a138ed68e7",
+    "url": "/website-portfolio/static/media/linkedInIcon.56b93e75.svg"
+  },
+  {
+    "revision": "b018dd4ee2594a6b20da9b88e21dd5e5",
+    "url": "/website-portfolio/static/media/mysqlIcon.b018dd4e.svg"
+  },
+  {
+    "revision": "705b987e58fa317cf913c181018040be",
+    "url": "/website-portfolio/static/media/nodeIcon.705b987e.svg"
+  },
+  {
+    "revision": "5d993cd681195888c002a03e9daf6c2c",
+    "url": "/website-portfolio/static/media/pythonIcon.5d993cd6.svg"
+  },
+  {
+    "revision": "4abe940d4759c1a9a8bd571a92721cb3",
+    "url": "/website-portfolio/static/media/reactIcon.4abe940d.svg"
+  }
+]);
